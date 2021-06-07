46d2f385a16d13036d4b472e8be72d84d6a6011f
Reed <reed.hunsaker@gmail.com>
2021-06-07 08:58:53 -0600

    Initial Commit

---
+++ LanguageAppWords/LanguageAppWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageAppWordsApp.swift
+//  LanguageAppWords
+//
+//  Created by Reed hunsaker on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct LanguageAppWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
